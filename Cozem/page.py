d87ccc4bc637272528ddea2ddc5280546b079f47
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-22 14:01:39 +0900

    수정

--- Cozem/page.py
+++ Cozem/page.py
@@ -155,7 +155,7 @@ elif choice == "길드페이지":
             ### 길드 간부진 💪
             | 직책 | 이름  | 직업 | 간부진 1:1오픈채팅 | 메지지 프로필 |
             | :---: | :---: | :---: | :---: | :---: |
-            | 길마👑 | 뱌닢 | 나이트로드 | [![maple](https://img.shields.io/badge/kakaotalk-뱌닢-yellow)](https://open.kakao.com/o/spPPOAhc) |[![maple](https://img.shields.io/badge/maplestory%20-%2314354C.svg?style=for-the-badge&logo=maplestory&logoColor=white](https://maple.gg/u/뱌닢) |
+            | 길마👑 | 뱌닢 | 나이트로드 | [![maple](https://img.shields.io/badge/kakaotalk-뱌닢-yellow)](https://open.kakao.com/o/spPPOAhc) |[![maple](https://img.shields.io/badge/maplestory%20-%2314354C.svg?style=for-the-badge&logo=maplestory&logoColor=white)](https://maple.gg/u/뱌닢) |
             | 부마 | 릎샴  | 아크 | [![maple](https://img.shields.io/badge/kakaotalk-릎샴-yellow)](https://open.kakao.com/o/s0FeFIee) | |
             | 부마 | 둥둥향 | 캐논슈터 | [![maple](https://img.shields.io/badge/kakaotalk-둥둥향-yellow)](https://open.kakao.com/o/sl6WBJUc) | |
             | 부마 | 돌체라페  | 메르세데스 | [![maple](https://img.shields.io/badge/kakaotalk-돌체라페-yellow)](https://open.kakao.com/o/sEmQw9Ye) | |
