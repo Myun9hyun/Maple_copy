693db79d4b6c1b969699423e653525b9af687ec3
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-17 13:16:14 +0900

    ㅇㅇ

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -730,6 +730,7 @@ elif choice == "아카이브":
 elif choice == "의견남기기":
     st.header("간부진들에게 하고싶은 말을 남겨주세요")
     st.write("남겨주신 의견은 간부진들만 확인하며, 남겨주신 내용을 바탕으로 더 나은 길드 만들겠습니다!")
+    st.write("의견 남겨주신 분들은 전원 코어젬스톤 5개씩 지급해드릴 예정입니다")
     FILE_PATH10 = 'data10.csv'
     options = ["의견 남기기➕", "내용 조회🔎", "내용 삭제✂", "초기화💣" ]
     option = st.selectbox("기능 선택", options, key='select3')
