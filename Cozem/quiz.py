f8dfd4ab737f445ca1d795d600817a27f23b3578
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-17 13:15:40 +0900

    dd

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -728,7 +728,7 @@ elif choice == "아카이브":
             st.image("Cozem/image/elinel.jpg", use_column_width=True)
 
 elif choice == "의견남기기":
-    st.write("간부진들에게 하고싶은 말을 남겨주세요")
+    st.header("간부진들에게 하고싶은 말을 남겨주세요")
     st.write("남겨주신 의견은 간부진들만 확인하며, 남겨주신 내용을 바탕으로 더 나은 길드 만들겠습니다!")
     FILE_PATH10 = 'data10.csv'
     options = ["의견 남기기➕", "내용 조회🔎", "내용 삭제✂", "초기화💣" ]
