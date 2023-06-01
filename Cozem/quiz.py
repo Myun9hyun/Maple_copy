a4d4263dc26c09fa31dc4b54e9d1f22aed30dc03
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-06-01 11:16:30 +0900

    창설이벤트 포스터 추가

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -273,6 +273,7 @@ elif choice == "퀴즈풀기":
         st.write("의견을 남겨준 길드원 전원에게 코어젬스톤💎 5개씩 지급할게!")
         st.write("Quiz탭에 들어가서 문제를 순서대로 풀면 돼!")
         st.write("첫번째 문제의 비밀번호는 공지방에 시간에 맞춰 공개될거야!")
+        st.image("Cozem/image/anniversary_event.jpeg", use_column_width=True)
         
         
         
