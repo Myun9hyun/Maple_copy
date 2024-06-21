5090281bba12d18180ff066164e8abe0fc3e7214
MyungHyun <126433920+Myun9hyun@users.noreply.github.com>
2024-06-21 14:13:51 +0900

    dlalwl

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -186,7 +186,7 @@ elif choice == "길드페이지":
         st.write("~https://maple.gg/~")
         st.write("~https://maplescouter.com/~")
         st.write("~https://cubemesu.co/~")
-        (https://img.shields.io/badge/maplestory%20-%2314354C.svg?style=for-the-badge&logo=maplestory&logoColor=white)(https://maple.gg/u/아기자기) 
+        st.image((https://img.shields.io/badge/maplestory%20-%2314354C.svg?style=for-the-badge&logo=maplestory&logoColor=white)(https://maple.gg/u/아기자기) )
 
 
 
