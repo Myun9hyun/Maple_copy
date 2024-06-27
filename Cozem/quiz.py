4fd38dee792096393eb631c335730eb74d5b812b
MyungHyun <126433920+Myun9hyun@users.noreply.github.com>
2024-06-27 16:24:43 +0900

    버튼

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -334,21 +334,14 @@ elif choice == "퀴즈풀기":
         if quiz1_password == password1:
             quiz1 = st.text_input("우리 길드의 이름은 아기자기이다!!")
             # if st.button("정답 확인", key="check_answer_button1"):
-            col1, col2 = st.columns(2)
-
-            # 첫 번째 열에 버튼 배치
-            with col1:
-                if st.button("O"):
-                    st.balloons()
-                    st.success("정답입니다!")
-                    st.write("우리 길드와 함께해줘서 고마워 ╰(*°▽°*)╯")
-                    st.write("2번 문제 오픈을 위한 비밀번호는 '초초' 야!")
-                    st.write("'초초'는 우리 아기자기 연합 길드로, 길드원들의 부캐릭터를 가입 시킬 수 있어!")
-
-            # 두 번째 열에 버튼 배치
-            with col2:
-                if st.button("X"):
-                    st.warning("다시 한 번 생각해봐!")
+            if st.button("O"):
+                st.balloons()
+                st.success("정답입니다!")
+                st.write("우리 길드와 함께해줘서 고마워 ╰(*°▽°*)╯")
+                st.write("2번 문제 오픈을 위한 비밀번호는 '초초' 야!")
+                st.write("'초초'는 우리 아기자기 연합 길드로, 길드원들의 부캐릭터를 가입 시킬 수 있어!")
+            if st.button("X"):
+                st.warning("다시 한 번 생각해봐!")
             if st.button("힌트 보기", key="check_hint_button1"):
                     st.write("이건 힌트를 줄 수가 없어! 잘 생각해 봐")
         elif quiz1_password != "" and quiz1_password != password1:
