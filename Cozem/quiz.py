eb0e3bbfe6b18807111023f27c6c0995e3196906
MyungHyun <126433920+Myun9hyun@users.noreply.github.com>
2024-06-28 12:37:03 +0900

    수정

--- Cozem/quiz.py
+++ Cozem/quiz.py
@@ -334,13 +334,13 @@ elif choice == "퀴즈풀기":
         if quiz1_password == password1:
             quiz1 = st.info("Q1. 우리 길드의 이름은 아기자기이다!!")
             # if st.button("정답 확인", key="check_answer_button1"):
-            if st.button("O", key = 'quiz1_button'):
+            if st.button("O", key = 'quiz1_O_button'):
                 st.balloons()
                 st.success("정답입니다!")
                 st.write("우리 아기자기와 함께해줘서 고마워 ╰(*°▽°*)╯")
                 st.write("2번 문제 오픈을 위한 비밀번호는 '초초' 야!")
                 st.write("'초초'는 우리 아기자기 연합 길드로, 길드원들의 부캐릭터를 가입 시킬 수 있어!")
-            if st.button("X"):
+            if st.button("X", key="quiz1_x_button"):
                 st.warning("다시 한 번 생각해봐!")
             if st.button("힌트 보기", key="check_hint_button1"):
                     st.info("이건 힌트를 줄 수가 없어! 잘 생각해 봐")
@@ -378,7 +378,7 @@ elif choice == "퀴즈풀기":
         quiz2_password = st.text_input("2번 문제 오픈을 위한 비밀번호를 입력해주세요!")
         if quiz2_password == password2:
             quiz2 = st.info("Q2. 아기자기 길드의 간부진은 총 4명이다!")
-            if st.button("O", key = "quiz2_button"):
+            if st.button("O", key = "quiz2_O_button"):
                 st.balloons()
                 st.success("정답입니다!")
                 
@@ -422,7 +422,7 @@ elif choice == "퀴즈풀기":
                 """
 
                 st.info(info_text)
-            if st.button("X"):
+            if st.button("X", key="quiz2_X_button"):
                     st.warning("다시 한 번 생각해봐!")
             if st.button("힌트 보기", key = "check_hint_button2"):
                 st.info("카카오톡 길드 공지방 멤버보기에서 왕관표시가 되어있는 인원수를 세어봐!")
